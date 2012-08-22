124ff30e070c0d25e0b68a2cc3110bf0e4c3be63
Travis Webb <travis@traviswebb.com>
2012-08-22 13:41:40 -0400

    version 0.1

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ javascript-maven-archetype
 ==========================
 
 Maven Archetype for Javascript Applications
+
+Usage: mvn archetype:generate -DarchetypeGroupId=nbt.tools -DarchetypeArtifactId=javascript-app-archetype -DarchetypeVersion=0.1
---
+++ src/main/resources/archetype-resources/src/main/js/index.js
@@ -0,0 +1,13 @@
+/*global $, require, window */
+
+// When the document is ready:
+$().ready(function() {
+
+	require([ "IndexContext" ], function(IndexContext) {
+		// Create a new IndexContext and initialize it which will create and
+		// start a HelloWorldController
+		var indexContext = new IndexContext();
+		indexContext.initialize();
+	});
+
+});
